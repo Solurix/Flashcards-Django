3ec2fc77dca932736f8a406e46da2e0c95cfd540
Bartosz <g.rapnal@gmail.com>
2020-07-15 16:09:09 +0900

    debug off

--- FCards/FCards/settings.py
+++ FCards/FCards/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '@29rj+wdpsns5m)x$0ud)k&ium_v*^ty3a6u5+7d8zoj1_h%j!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['solurix.pythonanywhere.com']
 
 
 # Application definition
