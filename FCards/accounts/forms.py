3019cb542c771f895d5ae3b49a0fee9dd1743a33
Bartosz <g.rapnal@gmail.com>
2020-07-22 20:00:06 +0900

    Created password recovery, Changed email to unique

--- FCards/accounts/forms.py
+++ FCards/accounts/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 
 
 class SignUpForm(UserCreationForm):
@@ -12,3 +15,8 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
 
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError((_(mark_safe("""An account already exists for this email address. <a href="/accounts/login/" class="email_error">Log in instead?</a> You can also <a href="/accounts/password_reset/" class="email_error">reset your password.</a>"""))))
+        return self.cleaned_data
