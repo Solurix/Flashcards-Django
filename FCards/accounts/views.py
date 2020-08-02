f0e8c7bdafa80ae30ebb428d9116b18c09d781d2
Bartosz <g.rapnal@gmail.com>
2020-08-02 13:54:50 +0900

    error500 fix

--- FCards/accounts/views.py
+++ FCards/accounts/views.py
@@ -29,7 +29,7 @@ def error404(request, exception):
     return render(request, 'errors/404.html', status=404)
 
 
-def error500(request, exception):
+def error500(request):
     return render(request, 'errors/500.html', status=500)
 
 
